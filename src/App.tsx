f47845a3cbf225cbd8c44d9373dd178fbcb0701a
laurensnol <laurensn@t-online.de>
2022-10-10 03:31:49 +0200

    Coming soon design

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="NavBar">
+        <h6>Laurens Nolting</h6>
+      </div>
+      <div className="Hero">
+        <h1>Coming Soon</h1>
+      </div>      
     </div>
   );
 }
