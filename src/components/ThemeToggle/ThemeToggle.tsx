d2d964e21581ade0365d02400b29ad756effb452
laurensnol <laurensn@t-online.de>
2022-10-15 17:17:54 +0200

    Updated ThemeToggle and fixed theme flickering

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -4,21 +4,25 @@ import { useState, useEffect } from 'react';
 import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
 
 function ThemeToggle() {
-  const [isDarkMode, setDarkMode] = useState(false);
+  var root = document.getElementsByTagName('html')[0];
+  const [isDarkMode, setDarkMode] = useState(root.classList.contains("dark"));
 
   useEffect(() => {
-    if (window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      setDarkMode(true);
-    }
-  }, [])
+    // Add some CSS to make later transitions between dark and light mode more smooth
+    var sheet = document.createElement('style');
+    sheet.innerText = "html { transition: 0.2s; }";
+    document.head.appendChild(sheet);
+  }, []);
 
   useEffect(() => {
-    var root = document.getElementsByTagName('html')[0];
+    console.log("Ran isDarkMode");
 
     if (isDarkMode) {
       root.setAttribute('class', 'dark');
+      localStorage.setItem('theme', 'dark');
     } else {
       root.removeAttribute('class');
+      localStorage.setItem('theme', 'light');
     }
   }, [isDarkMode])
 
